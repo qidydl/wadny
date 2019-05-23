efcf0024dcdcc167d766b6e7b32dc43d106dfe35
David Osolkowski <qidydl@gmail.com>
2019-05-23 19:38:51 -0400

    Move demo page so it doesn't conflict with intro

---
+++ src/design/edgeirc4/demo/head.php
@@ -0,0 +1,26 @@
+<?php echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<title>EdgeIRC Network - <?=$title?></title>
+	<meta name="author" content="David Osolkowski" />
+	<link rel="stylesheet" type="text/css" media="screen" href="/design/edgeirc4/demo/global.css" />
+</head>
+<body id="<?php echo strtolower($title); ?>">
+
+<div id="heading">
+	<h1>EdgeIRC</h1>
+	<h2>Bringing combat alfalfa to the masses since May 2000.</h2>
+</div>
+<ul id="menu">
+	<li><a href="/design/edgeirc4/demo/about/" id="labout" title="Some general information on EdgeIRC">About</a></li>
+	<li><a href="/design/edgeirc4/demo/channels/" id="lchannels" title="A list of some of the more popular channels">Channels</a></li>
+	<li><a href="/design/edgeirc4/demo/ircd/" id="lircd" title="The website for our very own IRC daemon">EdgeIRCD</a></li>
+	<li><a href="/design/edgeirc4/demo/ircops/" id="lircops" title="The people who run EdgeIRC">IRCops</a></li>
+	<li><a href="/design/edgeirc4/demo/news/" id="lnews" title="The latest EdgeIRC network news">News</a></li>
+	<li><a href="/design/edgeirc4/demo/other/" id="lother" title="Other information and links">Other</a></li>
+	<li><a href="/design/edgeirc4/demo/servers/" id="lservers" title="A list of currently active servers with information on connecting">Servers</a></li>
+	<li><a href="/design/edgeirc4/demo/socks/" id="lsocks" title="Information on our automatic socks-detection service">Socks</a></li>
+</ul>
+
+<div id="content">
