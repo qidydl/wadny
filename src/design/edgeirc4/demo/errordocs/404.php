9f1efc72caeab87052b6b2fab7fb16969ea4be55
David Osolkowski <qidydl@gmail.com>
2019-05-23 21:23:17 -0400

    Maybe it works now?

--- src/design/edgeirc4/demo/errordocs/404.php
+++ src/design/edgeirc4/demo/errordocs/404.php
@@ -1,9 +1,7 @@
 <?php
 	$title = "404 Not Found";
-	//include( "../head.php" );
+	include( "../head.php" );
 ?>
 <h3>404 - page not found</h3>
 <p>The page you requested, <?=$_SERVER["REQUEST_URI"]?>, does not exist on this server.  Thank you, drive through.</p>
-<?php
-	//include( "../foot.php" );
-?>
+<?php include( "../foot.php" ); ?>
