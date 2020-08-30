847b397d4a11ef6ae7927ad49864fa9cd614a350
David Osolkowski <qidydl@gmail.com>
2020-08-29 20:55:33 -0400

    Reduce use of server-side includes

--- src/design/summary/ydl-new/imgpreload.js
+++ src/design/summary/ydl-new/imgpreload.js
@@ -1,35 +1,33 @@
-<script language="JavaScript" type="text/javascript">
-<!--
-arImageSrc = new Array (
- "/design/ydl-new/news-sel.gif",
- "/design/ydl-new/people-sel.gif",
- "/design/ydl-new/about-sel.gif",
- "/design/ydl-new/mirc-sel.gif",
- "/design/ydl-new/idtscript-sel.gif",
- "/design/ydl-new/qidbot-sel.gif",
- "/design/ydl-new/email-sel.gif",
- "/design/ydl-new/blink-sel.gif",
- "/design/ydl-new/webdesign-sel.gif",
- "/design/ydl-new/addons-sel.gif",
- "/design/ydl-new/programming-sel.gif"
+arImageSrc = new Array(
+  "/design/summary/ydl-new/news-sel.gif",
+  "/design/summary/ydl-new/people-sel.gif",
+  "/design/summary/ydl-new/about-sel.gif",
+  "/design/summary/ydl-new/mirc-sel.gif",
+  "/design/summary/ydl-new/idtscript-sel.gif",
+  "/design/summary/ydl-new/qidbot-sel.gif",
+  "/design/summary/ydl-new/email-sel.gif",
+  "/design/summary/ydl-new/blink-sel.gif",
+  "/design/summary/ydl-new/webdesign-sel.gif",
+  "/design/summary/ydl-new/addons-sel.gif",
+  "/design/summary/ydl-new/programming-sel.gif"
 )
 
-arImageList = new Array ();
+arImageList = new Array();
 
 for (counter in arImageSrc) {
- arImageList[counter] = new Image();
- arImageList[counter].src = arImageSrc[counter];
+  arImageList[counter] = new Image();
+  arImageList[counter].src = arImageSrc[counter];
 }
 
-if (navigator.appName == "Microsoft Internet Explorer")
-  client = "ie";
-else
+if (document.layers)
   client = "ns";
+else
+  client = "ie";
 
 menuCurrent = "normal";
 menuMirc = "none";
 
-function MoveLayer(layer,top,left) {
+function MoveLayer(layer, top, left) {
   if (client == "ns") {
     document.layers[layer].top = top;
     document.layers[layer].left = left;
@@ -83,14 +81,14 @@ function ShowMenu(menu) {
     ShowLayer("qidbotImage");
     ShowLayer("blinkImage");
     ShowLayer("addonImage");
-    MoveLayer("emailImage",305,145);
-    MoveLayer("webImage",335,155);
-    MoveLayer("programImage",365,160);
+    MoveLayer("emailImage", 305, 145);
+    MoveLayer("webImage", 335, 155);
+    MoveLayer("programImage", 365, 160);
     ShowMircMenu(menuMirc);
   }
   if (menu == "webdesign") {
     ShowLayer("webMenu");
-    MoveLayer("programImage",255,130);
+    MoveLayer("programImage", 255, 130);
   }
   if (menu == "programming") { ShowLayer("programMenu"); }
 }
@@ -99,31 +97,31 @@ function ShowMircMenu(menu) {
     HideMircMenu("qidbot");
     HideMircMenu("blink");
     ShowLayer("idtMenu");
-    MoveLayer("qidbotImage",260,130);
-    MoveLayer("blinkImage",290,140);
-    MoveLayer("addonImage",320,150);
-    MoveLayer("emailImage",350,155);
-    MoveLayer("webImage",380,160);
-    MoveLayer("programImage",410,160);
+    MoveLayer("qidbotImage", 260, 130);
+    MoveLayer("blinkImage", 290, 140);
+    MoveLayer("addonImage", 320, 150);
+    MoveLayer("emailImage", 350, 155);
+    MoveLayer("webImage", 380, 160);
+    MoveLayer("programImage", 410, 160);
   }
   if (menu == "qidbot") {
     HideMircMenu("idtscript");
     HideMircMenu("blink");
     ShowLayer("qidbotMenu");
-    MoveLayer("blinkImage",280,140);
-    MoveLayer("addonImage",310,150);
-    MoveLayer("emailImage",340,155);
-    MoveLayer("webImage",370,160);
-    MoveLayer("programImage",400,160);
+    MoveLayer("blinkImage", 280, 140);
+    MoveLayer("addonImage", 310, 150);
+    MoveLayer("emailImage", 340, 155);
+    MoveLayer("webImage", 370, 160);
+    MoveLayer("programImage", 400, 160);
   }
   if (menu == "blink") {
     HideMircMenu("idtscript");
     HideMircMenu("qidbot");
     ShowLayer("blinkMenu");
-    MoveLayer("addonImage",310,150);
-    MoveLayer("emailImage",340,155);
-    MoveLayer("webImage",370,160);
-    MoveLayer("programImage",400,160);
+    MoveLayer("addonImage", 310, 150);
+    MoveLayer("emailImage", 340, 155);
+    MoveLayer("webImage", 370, 160);
+    MoveLayer("programImage", 400, 160);
   }
 }
 function HideMenu(menu) {
@@ -135,40 +133,40 @@ function HideMenu(menu) {
     HideLayer("qidbotImage");
     HideLayer("blinkImage");
     HideLayer("addonImage");
-    MoveLayer("emailImage",185,125);
-    MoveLayer("webImage",215,125);
-    MoveLayer("programImage",245,125);
+    MoveLayer("emailImage", 185, 125);
+    MoveLayer("webImage", 215, 125);
+    MoveLayer("programImage", 245, 125);
   }
   if (menu == "webdesign") {
     HideLayer("webMenu");
-    MoveLayer("programImage",245,125);
+    MoveLayer("programImage", 245, 125);
   }
   if (menu == "programming") { HideLayer("programMenu"); }
 }
 function HideMircMenu(menu) {
   if (menu == "idtscript") {
     HideLayer("idtMenu");
-    MoveLayer("qidbotImage",215,125);
-    MoveLayer("blinkImage",245,130);
-    MoveLayer("addonImage",275,135);
-    MoveLayer("emailImage",305,145);
-    MoveLayer("webImage",335,155);
-    MoveLayer("programImage",365,160);
+    MoveLayer("qidbotImage", 215, 125);
+    MoveLayer("blinkImage", 245, 130);
+    MoveLayer("addonImage", 275, 135);
+    MoveLayer("emailImage", 305, 145);
+    MoveLayer("webImage", 335, 155);
+    MoveLayer("programImage", 365, 160);
   }
   if (menu == "qidbot") {
     HideLayer("qidbotMenu");
-    MoveLayer("blinkImage",245,130);
-    MoveLayer("addonImage",275,135);
-    MoveLayer("emailImage",305,145);
-    MoveLayer("webImage",335,155);
-    MoveLayer("programImage",365,160);
+    MoveLayer("blinkImage", 245, 130);
+    MoveLayer("addonImage", 275, 135);
+    MoveLayer("emailImage", 305, 145);
+    MoveLayer("webImage", 335, 155);
+    MoveLayer("programImage", 365, 160);
   }
   if (menu == "blink") {
     HideLayer("blinkMenu");
-    MoveLayer("addonImage",275,135);
-    MoveLayer("emailImage",305,145);
-    MoveLayer("webImage",335,155);
-    MoveLayer("programImage",365,160);
+    MoveLayer("addonImage", 275, 135);
+    MoveLayer("emailImage", 305, 145);
+    MoveLayer("webImage", 335, 155);
+    MoveLayer("programImage", 365, 160);
   }
 }
 function ShowIDT() {
@@ -183,5 +181,3 @@ function ShowBlink() {
   ToggleMenu("mirc");
   ToggleMircMenu("blink");
 }
-// -->
-</script>
