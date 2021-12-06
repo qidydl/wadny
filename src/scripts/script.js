2bd52a899b0ec61d18277f2c6435abd28443e0f0
David Osolkowski <qidydl@gmail.com>
2021-12-05 21:57:11 -0500

    Widen layout, automatic season changes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -28,6 +28,26 @@ for (const heading of headings) {
         `<a class="heading-anchor" href="#${heading.id}" aria-labelledby="${heading.id}">🔗</a>`);
 }
 
+// Set body style to match the time of year
+const month = new Date().getMonth();
+const body = document.getElementsByTagName("body")[0];
+
+if (month === 2 || month === 3) {
+    // March (2), April (3) = early spring (no modifier required)
+} else if (month === 4) {
+    // May (4) = spring
+    body.classList.add("spring");
+} else if (month === 5 || month === 6 || month === 7) {
+    // June (5), July (6), August (7) = summer
+    body.classList.add("summer");
+} else if (month === 8 || month === 9 || month === 10) {
+    // September (8), October (9), November (10) = fall
+    body.classList.add("fall");
+} else if (month === 11 || month === 0 || month === 1) {
+    // December (11), January (0), February (1) = winter
+    body.classList.add("winter");
+}
+
 // Support main menu toggle on mobile
 function toggleNav() {
     const nav = document.querySelector("nav");
