3d52e98d7964ce02ec9d0e3f2ea077b35fdf690e
David Osolkowski <qidydl@gmail.com>
2026-09-20 12:11:12 -0400

    Clean up debug output

--- src/_plugins/headings.rb
+++ src/_plugins/headings.rb
@@ -38,7 +38,6 @@ Jekyll::Hooks.register [:documents, :pages], :post_render do |page|
     end
 
     doc.css("table").each do |table|
-        puts "Wrapping table in " + page.inspect
         table.wrap("<div class=\"table\"></div>")
     end
 
